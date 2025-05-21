0d065168e564b530dac5b5ea40d7c2ad0ae7d7ed
Yehorka <25kabachok25@gmail.com>
2025-05-21 17:08:42 +0300

    main.tsx change

--- src/main.tsx
+++ src/main.tsx
@@ -3,5 +3,10 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import './i18n/i18n.ts'
+import { HashRouter } from 'react-router-dom'
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <HashRouter>
+    <App />
+  </HashRouter>
+);
